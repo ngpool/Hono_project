e7133f4538650d3447735fe60cd5c05c6fcade8f
N型 <u11b146h@gmail.com>
2024-12-01 19:53:40 +0900

    CRUD完成

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import posts from "./blogs/blogs";
+import auth from "./auth/auth";
+import { basicAuth } from 'hono/basic-auth';
 
 const app = new Hono()
-
+app.use(
+  '/auth/*',
+  basicAuth({
+    username: 'hono',
+    password: 'hono',
+  })
+)
+app.route("/posts", posts)
+app.route("/auth", auth)
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
